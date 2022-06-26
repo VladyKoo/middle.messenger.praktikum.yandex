02ece8f7d69f49d0c100f04c833783aad7b982f6
vladykoo <vladykoo@mail.ru>
2022-06-26 14:48:00 +0300

    create Router

---
+++ src/utils/Router.ts
@@ -0,0 +1,87 @@
+import { createRoot, Root } from './createRoot';
+
+export type Route = {
+  path: string;
+  component: new (...args: any[]) => any;
+  props?: any;
+  name?: string;
+  children?: Route[];
+};
+
+export class Router {
+  private readonly history: History;
+
+  private routes: Route[] = [];
+
+  private currentRoute: Route | null = null;
+
+  private currentComponent: any | null = null;
+
+  private root: Root;
+
+  constructor(routes: Route[], selector: string) {
+    this.history = window.history;
+    this.routes = routes;
+
+    this.root = createRoot(document.querySelector(selector));
+
+    this.init();
+  }
+
+  private init() {
+    window.addEventListener('popstate', () => {
+      const path = window.location.pathname;
+
+      this.onRoute(path);
+    });
+    this.onRoute(window.location.pathname);
+  }
+
+  private getRoute(path: string) {
+    let route = this.routes.find((item) => item.path === path);
+
+    if (!route) {
+      route = this.routes.find((item) => item.path === '*');
+    }
+
+    return route;
+  }
+
+  private onRoute(path: string) {
+    const route = this.getRoute(path);
+    if (!route) {
+      return;
+    }
+
+    if (this.currentRoute) {
+      this.currentComponent.dispatchComponentWillDestroy();
+      this.currentComponent = null;
+    }
+
+    this.currentRoute = route;
+    this.currentComponent = new this.currentRoute.component(this.currentRoute.props || {});
+
+    this.root.render(this.currentComponent);
+  }
+
+  public getPath(): string | null {
+    return this.currentRoute?.path || null;
+  }
+
+  public go(path: string | number) {
+    if (typeof path === 'number') {
+      this.history.go(path);
+    } else {
+      this.history.pushState('', '', path);
+      this.onRoute(path);
+    }
+  }
+
+  public back() {
+    this.history.back();
+  }
+
+  public forward() {
+    this.history.forward();
+  }
+}
