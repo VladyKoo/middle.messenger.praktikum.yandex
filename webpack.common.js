5a56f57a0fa28d447875fad4c293917da8a9733f
vladykoo <vladykoo@mail.ru>
2022-08-14 20:19:54 +0300

    divide webpack config into prod and dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === 'development';
-
 module.exports = {
   entry: './src/index.ts',
-  mode: process.env.NODE_ENV,
-  devtool: 'inline-source-map',
   output: {
     filename: '[name].[contenthash:8].js',
     path: path.resolve(__dirname, 'dist'),
@@ -18,10 +13,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './static/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDev ? '[name].css' : '[name].[contenthash:8].css',
-      chunkFilename: isDev ? '[id].css' : '[id].[contenthash:8].css',
     }),
     new CopyPlugin({
       patterns: [
@@ -34,36 +25,8 @@ module.exports = {
       ],
     }),
   ],
-  devServer: {
-    port: 5000,
-    hot: true,
-    historyApiFallback: true,
-  },
   module: {
     rules: [
-      {
-        test: /\.(scss|sass|css)$/,
-        use: [
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                auto: true,
-                localIdentName: isDev ? '[name]__[local]--[hash:base64:5]' : '[hash:base64]',
-                localIdentContext: path.resolve(__dirname, 'src'),
-              },
-              sourceMap: isDev,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDev,
-            },
-          },
-        ],
-      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
